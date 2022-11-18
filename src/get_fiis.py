8194642458d14d971656af711c18813c1cd970e9
vanderci curvelo jr <vandercijr@yahoo.com.br>
2022-11-18 17:59:18 -0300

    feat: add dry refactoring && add regex

--- src/get_fiis.py
+++ src/get_fiis.py
@@ -1,52 +1,47 @@
 
 import pandas as pd
 import requests
-import re
 from bs4 import BeautifulSoup
 
 import warnings
 warnings.filterwarnings("ignore")
 
 
-def get_fundos():
+def get_data():
     """
         Return a dataframe with FIIS (Fundos Imobiliários) coletados da plataforma Funds Explorer
     """
 
     url = 'https://www.fundsexplorer.com.br/ranking'
-    agent = {"User-Agent": "Mozilla/5.0"}
-    resposta = requests.get(url, headers=agent)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    resposta = requests.get(url, headers)
     soup = BeautifulSoup(resposta.text, 'lxml')
-    tabela = soup.find_all('table')[0]
+    return soup.find_all('table')[0]
 
+
+def get_fundos():
+    tabela = get_data()
     df = pd.read_html(str(tabela), decimal=',', thousands='.')[0]
 
     colunas = list(df.columns)
 
     for coluna in colunas:
         try:
-            df[coluna] = df[coluna].apply(
-                lambda s: re.sub(r'[R$.%]', r'', str(s))
-            )
-            df[coluna] = df[coluna].apply(
-                lambda s: re.sub(r',', r'.', str(s))).astype(float)
-
+            if coluna not in ['Setor', 'Códigodo fundo']:
+                df[coluna] = df[coluna] = df[coluna].str.replace(r"((?!(,|[0-9])).)", '', regex=True)
+            df[coluna] = df[coluna].str.replace(',', '.').astype(float)
+              
         except:
             continue
 
-    return df
+    return df.fillna(0)
 
 
 def get_fundos_symbols():
     """
         Return a list of FIIS
     """
-
-    url = 'https://www.fundsexplorer.com.br/ranking'
-    agent = {"User-Agent": "Mozilla/5.0"}
-    resposta = requests.get(url, headers=agent)
-    soup = BeautifulSoup(resposta.text, 'lxml')
-    tabela = soup.find_all('table')[0]
+    tabela = get_data()
 
     df = pd.read_html(str(tabela), decimal=',', thousands='.')[0]
 
